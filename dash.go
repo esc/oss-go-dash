745600ab08b01fc0384687e0456e5b957880acf6
Valentin Haenel <valentin@haenel.co>
2016-02-09 14:03:36 +0100

    switch to authenticated access with my private access token

--- dash.go
+++ dash.go
@@ -1,7 +1,9 @@
 package main
 
+import "io/ioutil"
 import "sort"
 import "github.com/google/go-github/github"
+import "golang.org/x/oauth2"
 
 type ByName []github.Repository
 
@@ -9,10 +11,7 @@ func (a ByName) Len()          int  {return len(a)}
 func (a ByName) Swap(i, j int)      {a[i], a[j] = a[j], a[i]}
 func (a ByName) Less(i, j int) bool {return *a[i].Name < *a[j].Name}
 
-func main() {
-    client := github.NewClient(nil)
-    org, _, _ := client.Organizations.Get("immobilienscout24")
-    println("Listing repositories for organization: ", *org.Login)
+func getAllRepos(client *github.Client, org *github.Organization) []github.Repository {
     opt := &github.RepositoryListByOrgOptions{ListOptions: github.ListOptions{PerPage: 10},}
     var allRepos []github.Repository
     for {
@@ -28,6 +27,30 @@ func main() {
         opt.ListOptions.Page = resp.NextPage
     }
     sort.Sort(ByName(allRepos))
+    return allRepos
+}
+
+func readTokenFromFile() string {
+    dat, _ := ioutil.ReadFile("token")
+    return string(dat)
+}
+
+func main() {
+    var token string = readTokenFromFile()
+    ts := oauth2.StaticTokenSource(
+        &oauth2.Token{AccessToken: token},
+    )
+    tc := oauth2.NewClient(oauth2.NoContext, ts)
+    client := github.NewClient(tc)
+    org, _, err := client.Organizations.Get("immobilienscout24")
+    if err != nil {
+        println("Failed to fetch organization from github")
+        println(err.Error())
+        return
+    }
+    println("Listing repositories for organization: ", *org.Login)
+
+    var allRepos []github.Repository = getAllRepos(client, org)
     for i := 0; i < len(allRepos); i++ {
         println(*allRepos[i].Name)
     }
